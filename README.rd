52bed5a5a116ccc26b2a556c46932013c55169ed
Amy Hwang <amyhwang1218@gmail.com>
2016-10-28 11:10:43 -0700

    add forEach loop to print thelist of numbers on the webpage

--- README.rd
+++ README.rd
@@ -1,6 +1,13 @@
 spces
-V * input is not a number (in: "Hi!" out: "Enter a number." )
-* number is divisible by 3 but not 5 (in: 9 out: ping)
-* number is divisible by 5 but not 3 (in: 20 out: pong)
-* number is divisible by 15 (in: 30 out: pingpong)
-* number is not divisible by 3 or 5 (in: 14 out: 14)
+* input is not a number
+  * input: "Hi!" output: "The input you have entered is not a number. Please enter a number."
+* input is less than or equal to 0
+  *input: 0, -10 output: "The input you have entered is less than or equal to 0. Please enter a number bigger than 0."
+* number is divisible by 3 but not 5
+  * input: 9 output: "ping"
+* number is divisible by 5 but not 3
+  * input: 20 output: "pong"
+* number is divisible by 15
+  * input: 30 output: "ping-pong"
+* number is not divisible by 3 or 5
+  * input: 14 output: 14
--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,50 @@
-var pingpongFunction = function(input) {
-  //If input is not a number, return an error message.
-  if (isNaN(input )) {
-    alert("The input you have submit is not a number. Please enter a number.")
+var pingPongFunction = function(input) {
+  if (isNaN(input)) {
+    alert("The input you have entered is not a number. Please enter a number.");
+  } else if (input <= 0 ) {
+    alert("The input you have entered is less than or equal to 0. Please enter a number bigger than 0.");
   } else {
-    //array of numbers from 1 to input
-    var numberRange = []
+    var numbersArray = [];
     for(var i = 1; i <= input; i++) {
-      numberRange.push(i);
+      numbersArray.push(i);
     }
-    //replace 3x = ping, 5x = pong, 15x = pingpong
-    var pingPongNumberRange = numberRange.map(function(number) {
-      if (number%3 === 0 && number%5 != 0) {
-        return "ping";
-      } else if (number%5 === 0 && number%3 != 0) {
-        return "pong"
-      } else if (number%3 === 0 && number%5 === 0) {
-        return "ping-pong"
-      } else {
-        return number;
-      }
-    });
-    console.log(pingPongNumberRange);
-  };
+  }
+  var pingPongNumbersArray = numbersArray.map(function(number) {
+    return pingPongNumbers(number);
+  });
+  return pingPongNumbersArray;
 };
+
+// var element = ""
+// for(var p = 0; p < pingPongNumbersArray.length; p++) {
+//   element = pingPongNumbersArray[p];
+// }
+// console.log(element);
+
+//replace 3x = ping, 5x = pong, 15x = ping-pong
+var pingPongNumbers = function(number) {
+  if (number%3 === 0 && number%5 != 0) {
+    return "ping";
+  } else if (number%5 === 0 && number%3 != 0) {
+    return "pong"
+  } else if (number%3 === 0 && number%5 === 0) {
+    return "ping-pong"
+  } else {
+    return number;
+  }
+}
+
+//each element in the array is printed on <li>
+//("li").text(pingPongFunction(input))
 
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("#input").val());
-    pingpongFunction(input);
-  })
-})
+    var outputList= $(".output-list");
+    console.log(pingPongFunction(input));
+    pingPongFunction(input).forEach(function(element) {
+      $(".output-list").append("<li>" + element + "</li>");
+    })
+  });
+});
