1ae0d54b04faddf7463716f1f561583855f6bd3b
Amy Hwang <amyhwang1218@gmail.com>
2016-10-28 14:48:07 -0700

    add styles like jombotron panel and sidebar

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
+//Business Logic
 var pingPongFunction = function(input) {
   if (isNaN(input) || input <= 0) {
     alert("The input you have entered is invalid. Please enter a number greater than 0.");
   } else {
+    //sets an array that contains a range of number from 0 to input.
     parseInt(input);
     var numbersArray = [];
     for(var i = 1; i <= input; i++) {
       numbersArray.push(i);
     }
+    //modifies the numbersArray to replace 3x to ping, 5x to pons, and 15x to ping-pong
+    var pingPongNumbersArray = numbersArray.map(function(number) {
+      return pingPongNumbers(number);
+    });
   }
-  var pingPongNumbersArray = numbersArray.map(function(number) {
-    return pingPongNumbers(number);
-  });
   return pingPongNumbersArray;
 };
 
-//replace 3x = ping, 5x = pong, 15x = ping-pong
+//replaces 3x = ping, 5x = pong, 15x = ping-pong
 var pingPongNumbers = function(number) {
   if (number%3 === 0 && number%5 != 0) {
     return "ping";
@@ -27,14 +30,15 @@ var pingPongNumbers = function(number) {
   }
 }
 
-//i want the new result to come out but not append on the last result.
-
+//User Interface Logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     var input = $("#input").val();
     var outputList = $(".output-list");
+    //clears the previous result
     $(".output-list").html("");
+    //lists the items inside pingPongNumbersArray
     pingPongFunction(input).forEach(function(item) {
       $(".output-list").append("<li>" + item + "</li>");
     });
